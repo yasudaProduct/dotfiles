ba565cfc1fa89429419bdfec38f0f18a760d44db
yasudaProduct <yasu.private.ct@gmail.com>
2026-01-30 22:28:29 +0900

    add raycast script

---
+++ .raycast-scripts/switchaudiosource-SOUNDPEATSPearlClipPro.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title SwitchAudioSource-Scarlett-2i2-USB
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author hisasann
+# @raycast.authorURL https://raycast.com/hisasann
+
+SwitchAudioSource -f json -t output -s "SOUNDPEATS PearlClip Pro"
